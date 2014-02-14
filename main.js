200131ed6667d66ba7471a5acbcffb24ab23d168
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2014-02-15 02:23:06 +1100

    add tilesprite asset floor

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-var game = new Phaser.Game(480, 320, Phaser.AUTO, '', {preload: preload, create: create, update: update});
+var game = new Phaser.Game(320, 480, Phaser.AUTO, '', {preload: preload, create: create, update: update});
 
 var player;
 var gravity = 5;
 var velocity = 0;
 var mouseReleased = true;
+var tilesprite;
 
 var tube;
 var tubeHeight = 200;
@@ -18,17 +19,20 @@ function preload(){
   game.load.image('bottom', 'bottom.png');
   game.load.image('top', 'top.png');
   game.load.image('flappy', 'flappy.png');
+  game.load.image('tile', 'tile.jpg');
 }
 
 function create(){
   game.stage.backgroundColor = '#ff00ff';
 
-  player = game.add.sprite(0,0,'flappy');
+  player = game.add.sprite(50,0,'flappy');
+  tilesprite = game.add.tileSprite(0, game.height - 50, game.width, 50, 'tile');
 
   game.time.events.loop(Phaser.Timer.SECOND * secondsBetweenTubes, createTube, this);
 }
 
 function update(){
+  tilesprite.tilePosition.x -= game.width * game.time.elapsed / 1000 / secondsBetweenTubes / secondsBetweenTubes;
 
   if(velocity > 0){
     // duration of the parabole higher = longer
@@ -48,6 +52,9 @@ function update(){
 
 document.onclick = function(){
   velocity = initialThrust;
+  player.rotation = -0.5;
+  player.anchor.setTo(1, 0);
+  console.log(player.body.rotation);
 };
 
 function createTube(){
