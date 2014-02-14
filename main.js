8993b5f577d99f77e97c1f3a73c2ea9754d0296b
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2014-02-15 00:07:23 +1100

    Basic physics for Zozio with approximatif thrust

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 var game = new Phaser.Game(480, 320, Phaser.AUTO, '', {preload: preload, create: create, update: update});
 
 var player;
-var velocity = 5;
+var tube;
+var gravity = 5;
+var velocity = 0;
+var mouseReleased = true;
+
+var jumpDuration = 50;
+var initialThrust = 15;
 
 function preload(){
 
@@ -9,12 +15,25 @@ function preload(){
 
 function create(){
   game.stage.backgroundColor = '#ff00ff';
+
   player = game.add.sprite(0,0,'player');
+  tube = game.add.sprite(100,0,'tube');
 }
 
 function update(){
-  player.body.velocity.y += velocity;
 
-  
-  // player
+  if(velocity > 0){
+    // duration of the parabole higher = longer
+    velocity -= game.time.elapsed / jumpDuration;
+  }else{
+    velocity = 0;
+  }
+
+  player.body.y += gravity - velocity;
+  // console.log(player.body.y);
+  // player.body.gravity.y += gravity;
 }
+
+document.onclick = function(){
+  velocity = initialThrust;
+};
