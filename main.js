bd4d7c230bd939505c1cb6387a4b54c9f8069fbe
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2014-02-15 03:04:16 +1100

    change height tubes

--- main.js
+++ main.js
@@ -7,13 +7,15 @@ var mouseReleased = true;
 var tilesprite;
 
 var tube;
-var tubeHeight = 200;
+var tubeHeight = 250;
 var tubeTab = [];
 
-var jumpDuration = 50;
+var jumpDuration = 30;
 var initialThrust = 15;
-var spaceBetweenTubes = 32;
-var secondsBetweenTubes = 2;
+var spaceBetweenTubes = 50;
+var secondsBetweenTubes = 3;
+
+var layer;
 
 function preload(){
   game.load.image('bottom', 'bottom.png');
@@ -28,11 +30,17 @@ function create(){
   player = game.add.sprite(50,0,'flappy');
   tilesprite = game.add.tileSprite(0, game.height - 50, game.width, 50, 'tile');
 
-  game.time.events.loop(Phaser.Timer.SECOND * secondsBetweenTubes, createTube, this);
+  layer = game.add.group();
+
+  layer.add(player);
+  layer.add(tilesprite);
+
+  game.time.events.loop(Phaser.Timer.SECOND * secondsBetweenTubes / 2, createTube, this);
 }
 
 function update(){
-  tilesprite.tilePosition.x -= game.width * game.time.elapsed / 1000 / secondsBetweenTubes / secondsBetweenTubes;
+  tilesprite.tilePosition.x -= game.width * game.time.elapsed / ( 1000 * secondsBetweenTubes ) ;
+  // gravity += 0.1;
 
   if(velocity > 0){
     // duration of the parabole higher = longer
@@ -44,29 +52,38 @@ function update(){
   player.body.y += gravity - velocity;
 
   for (var i = 0, il = tubeTab.length; i < il; i++) {
-    tubeTab[i].body.x -= game.width * game.time.elapsed / 1000 / secondsBetweenTubes / secondsBetweenTubes;
+    tubeTab[i].body.x -= game.width * game.time.elapsed / ( 1000 * secondsBetweenTubes );
   }
+
+  layer.bringToTop(tilesprite);
   // console.log(player.body.y);
   // player.body.gravity.y += gravity;
 }
 
 document.onclick = function(){
   velocity = initialThrust;
-  player.rotation = -0.5;
-  player.anchor.setTo(1, 0);
-  console.log(player.body.rotation);
+  // gravity = 5;
+  // player.rotation = -0.5;
+  // player.anchor.setTo(1, 0);
+  // console.log(player.body.rotation);
 };
 
 function createTube(){
 
   //              round to <     random                  until           from
-  var placement = Math.floor( Math.random() * ( tubeHeight + spaceBetweenTubes - 88) ) + 88;
+  var placement = Math.floor( Math.random() * ( tubeHeight + spaceBetweenTubes - 150) ) + 150;
 
   /*
-    min placement = 88  = (32 + 64 - 8) ( idk why )
-    max placement = 232 = (200 + 32) = (height of tube + ( space between placement and tube / 2 ) )
+    min placement = 88  = (spaceBetweenTubes + 100 - 8) ( idk why )
+    max placement = 232 = (200 + spaceBetweenTubes) = (height of tube + ( space between placement and tube / 2 ) )
   */
 
-  tubeTab.push(game.add.sprite(game.width, placement - spaceBetweenTubes - tubeHeight, 'top'));
-  tubeTab.push(game.add.sprite(game.width, placement + spaceBetweenTubes, 'bottom'));
+  var tube1 = game.add.sprite(game.width, placement + spaceBetweenTubes, 'bottom');
+  var tube2 = game.add.sprite(game.width, placement - spaceBetweenTubes - tubeHeight, 'top');
+
+  layer.add(tube1);
+  layer.add(tube2);
+
+  tubeTab.push(tube1);
+  tubeTab.push(tube2);
 }
