3b71c4a046a6d08483b3238b3261e3c8d576ef4d
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2014-02-15 13:39:23 +1100

    add overlay when collide

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(320, 480, Phaser.AUTO, '', {preload: preload, create: create, update: update});
+var game = new Phaser.Game(320, 480, Phaser.AUTO, 'wrapper', {preload: preload, create: create, update: update});
 
 var player;
 var gravity = 5;
@@ -16,7 +16,12 @@ var initialThrust = 13;
 var spaceBetweenTubes = 50;
 var secondsBetweenTubes = 2.5;
 
-var layer;
+var frontLayer;
+var tubeLayer;
+var collision;
+
+var overlay = document.getElementById('overlay');
+var firstFrameTouched = false;
 
 function preload(){
   game.load.image('bottom', 'bottom.png');
@@ -31,34 +36,54 @@ function create(){
   player = game.add.sprite(50,0,'flappy');
   tilesprite = game.add.tileSprite(0, game.height - 50, game.width, 50, 'tile');
 
-  layer = game.add.group();
+  tubeLayer = game.add.group();
+  frontLayer = game.add.group();
 
-  layer.add(player);
-  layer.add(tilesprite);
+  frontLayer.add(tilesprite);
+  frontLayer.add(player);
 
   game.time.events.loop(Phaser.Timer.SECOND * secondsBetweenTubes / 2, createTube, this);
   game.time.events.loop(Phaser.Timer.SECOND, removeTubes, this);
 }
 
 function update(){
-  tilesprite.tilePosition.x -= game.width * game.time.elapsed / ( 1000 * secondsBetweenTubes ) ;
+  if(firstFrameTouched){
+    overlay.style.display = 'none';
+  }
+
   gravity += 0.05;
 
   if(velocity > 0){
-    // duration of the parabole higher = longer
     velocity -= game.time.elapsed / jumpDuration;
   }else{
     velocity = 0;
   }
 
-  player.body.y += gravity - velocity;
 
-  for (var i = 0, il = tubeTab.length; i < il; i++) {
-    tubeTab[i].body.x -= game.width * game.time.elapsed / ( 1000 * secondsBetweenTubes );
+  if(!collision){
+    for (var i = 0, il = tubeTab.length; i < il; i++) {
+      tubeTab[i].body.x -= game.width * game.time.elapsed / ( 1000 * secondsBetweenTubes );
+    }
+    tilesprite.tilePosition.x -= game.width * game.time.elapsed / ( 1000 * secondsBetweenTubes ) ;
   }
 
-  layer.bringToTop(tilesprite);
+  frontLayer.bringToTop(tilesprite);
+  frontLayer.bringToTop(player);
+
   // player.body.gravity.y += gravity;
+  game.physics.overlap(player, tubeLayer, collisionTube, null, this);
+  // game.physics.overlap(player, tilesprite, collisionFloor, null, this);
+
+  if(player.body.y + player.body.height >= game.height - 52){
+    if(!firstFrameTouched){
+      console.log('floor');
+      overlay.style.display = 'block';
+      firstFrameTouched = true;
+    }
+    collision = true;
+  }else{
+    player.body.y += gravity - velocity;
+  }
 }
 
 document.onclick = function(){
@@ -72,18 +97,18 @@ document.onclick = function(){
 function createTube(){
 
   //              round to <     random                  until           from
-  var placement = Math.floor( Math.random() * ( tubeHeight + spaceBetweenTubes - 150) ) + 150;
+  // var placement = Math.floor( Math.random() * ( tubeHeight + spaceBetweenTubes - 150) ) + 150;
 
   /*
-    min placement = 88  = (spaceBetweenTubes + 100 - 8) ( idk why )
-    max placement = 232 = (200 + spaceBetweenTubes) = (height of tube + ( space between placement and tube / 2 ) )
+    min placement = 82 
+    max placement = 232
   */
-
+  var placement = 130;
   var tube1 = game.add.sprite(game.width, placement + spaceBetweenTubes, 'bottom');
   var tube2 = game.add.sprite(game.width, placement - spaceBetweenTubes - tubeHeight, 'top');
 
-  layer.add(tube1);
-  layer.add(tube2);
+  tubeLayer.add(tube1);
+  tubeLayer.add(tube2);
 
   tubeTab.push(tube1);
   tubeTab.push(tube2);
@@ -93,13 +118,31 @@ function removeTubes(){
   if(tubeTab.length){
     if(tubeTab[0].body.x < -54 ){
       tubeTab[0].kill();
-      layer.remove(tubeTab[0]);
+      tubeLayer.remove(tubeTab[0]);
       tubeTab.splice(0, 1);
     }
     if(tubeTab[1].body.x < -54){
       tubeTab[1].kill();
-      layer.remove(tubeTab[1]);
+      tubeLayer.remove(tubeTab[1]);
       tubeTab.splice(1, 1);
     }
   }
+}
+
+function collisionTube(obj1, obj2){
+  if(!firstFrameTouched){
+    console.log('tube');
+    overlay.style.display = 'block';
+    firstFrameTouched = true;
+  }
+  collision = true;
+}
+
+function collisionFloor(obj1, obj2) {
+  // if(!firstFrameTouched){
+  //   console.log('floor');
+  //   overlay.style.display = 'block';
+  //   firstFrameTouched = true;
+  // }
+  // collision = true;
 }
