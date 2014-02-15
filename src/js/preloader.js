5817c20d6e184c7c18911d418153b8c7ef9fd7b5
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2014-02-15 17:21:01 +1100

    seems to work with states chang, needed to change phaser ! to 1.5

---
+++ src/js/preloader.js
@@ -0,0 +1,48 @@
+(function() {
+  'use strict';
+
+  function Preloader() {
+    this.asset = null;
+    this.ready = false;
+  }
+
+  Preloader.prototype = {
+
+    preload: function () {
+      this.asset = this.add.sprite(320, 240, 'preloader');
+      this.asset.anchor.setTo(0.5, 0.5);
+
+      this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
+      this.load.setPreloadSprite(this.asset);
+      this.load.image('player', 'assets/player.png');
+      this.load.bitmapFont('minecraftia', 'assets/minecraftia.png', 'assets/minecraftia.xml');
+
+      this.load.image('bottom', 'assets/bottom.png');
+      this.load.image('top', 'assets/top.png');
+      this.load.image('flappy', 'assets/flappy.png');
+      this.load.image('tile', 'assets/tile.jpg');
+
+      this.load.image('play', 'assets/play.png');
+      this.load.image('score', 'assets/score.png');
+      this.load.image('pause', 'assets/pause.jpg');
+    },
+
+    create: function () {
+      this.asset.cropEnabled = false;
+    },
+
+    update: function () {
+      if (!!this.ready) {
+        this.game.state.start('menu');
+      }
+    },
+
+    onLoadComplete: function () {
+      this.ready = true;
+    }
+  };
+
+  window[''] = window[''] || {};
+  window[''].Preloader = Preloader;
+
+}());
